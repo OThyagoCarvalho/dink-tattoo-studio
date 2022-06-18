d9471b646b4e9eb884aa9b2407442c303c9965cc
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-06-18 10:55:11 -0300

    initial commit

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'DM Sans', sans-serif;
+    box-sizing: border-box;    
+  }
+`;
+
+
+export default GlobalStyle;
